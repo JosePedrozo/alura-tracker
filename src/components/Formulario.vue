f86778457f451bdb349e6cf59e536a45df514aba
José Alfredo Pedrozo <jose.pedrozo@dotgroup.com.br>
2023-11-08 20:30:25 -0300

    - Inicia projeto Alura-Tracker

---
+++ src/components/Formulario.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulário para uma criação de uma nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você pretende iniciar?"
+          v-model="descricao"
+        />
+      </div>
+      <div class="column">
+        <Temporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Temporizador from "./Temporizador.vue";
+export default defineComponent({
+    name: `Formulario`,
+    components: {
+        Temporizador
+    },
+    data () {
+      return {
+        descricao: ''
+      }
+    },
+    methods: {
+      finalizarTarefa (tempoDecorrido: number) : void {
+        console.log(tempoDecorrido);
+        this.descricao = ''
+      }
+    }
+});
+</script>
